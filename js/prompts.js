1c1fcd52d884a6093a3736dd321bc81c0ed7ffab
Dinsha911 <din911sha@gmail.com>
2026-08-09 21:22:18 +0300

    Update prompts.js

--- js/prompts.js
+++ js/prompts.js
@@ -196,51 +196,6 @@ searchBox.addEventListener(
    COPY PROMPT
 ========================= */
 
-function addCopyButtons() {
-
-    const buttons =
-        document.querySelectorAll(
-            ".copy-button"
-        );
-
-
-    buttons.forEach(function (button) {
-
-        button.addEventListener(
-            "click",
-            async function () {
-
-                const prompt =
-                    decodeURIComponent(
-                        button.dataset.prompt
-                    );
-
-
-                await navigator.clipboard.writeText(
-                    prompt
-                );
-
-
-                button.textContent =
-                    "✓ Copied!";
-
-
-                setTimeout(
-                    function () {
-
-                        button.textContent =
-                            "Copy Prompt";
-
-                    },
-                    2000
-                );
-
-            }
-        );
-
-    });
-
-}
 
 
 /* =========================
